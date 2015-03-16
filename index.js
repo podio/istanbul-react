1d37ea22bf4a5ec24591538250c61133c30ea50e
Søren Brokær <rasmussen@podio.com>
2015-03-16 17:54:20 +0100

    Initial implementation

---
+++ index.js
@@ -0,0 +1,33 @@
+var util = require('util');
+
+var istanbul = require('istanbul');
+
+var esprima = require('esprima');
+var reactTools = require('react-tools');
+
+function Instrumenter(opt){
+  istanbul.Instrumenter.call(this, opt);
+}
+
+util.inherits(Instrumenter, istanbul.Instrumenter);
+
+Instrumenter.prototype.instrument = function (content, file, callback) {
+  var jsPath = file.originalPath.replace(basePath + '/', './');
+
+  var transformed = reactTools.transformWithDetails(content, {
+    sourceMap: true,
+    harmony: true
+  });
+
+  var program = esprima.parse(transformed.code, {
+    loc: true
+  });
+
+  try {
+    callback(void 0, instrumenter.instrumentASTSync(program, jsPath, content));
+  } catch (exception) {
+    callback(exception);
+  }
+};
+
+module.exports = Instrumenter;
